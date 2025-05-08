7b7bfe1edccee0b009ab265562b40744e2882d58
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-05-08 14:17:45 -0300

    first commit

---
+++ src/main/java/com/mysite/dsprojectspring3/Dsprojectspring3Application.java
@@ -0,0 +1,13 @@
+package com.mysite.dsprojectspring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsprojectspring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsprojectspring3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/dsprojectspring3/Dsprojectspring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.dsprojectspring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsprojectspring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
